f9ed61695223a49125222924427c9930ba8b0e8c
Cade Tanner <cade@tannernet.net>
2025-10-03 00:36:41 -0700

    Task 18-Added Scriptable Object for Art data

---
+++ Assets/Scripts/ArtworkData.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewScriptableObjectScript", menuName = "Scriptable Objects/NewScriptableObjectScript")]
+public class NewScriptableObjectScript : ScriptableObject
+{
+    [SerializedField] public String artworkName; //Use this field for the artwork's name
+    [SerializedField] public String artistName; //Use this field for the artist's name
+    [SerializedField] public String artworkDescription; //Use this field for the artwork description
+    [SerializedField] public String genre; //Use this field for the artwork genre
+    [SerializedField] public String artistInformation; //Use this field for any extra information about the artist
+    [SerializedField] public String artworkDate; //Use this field for MONTH - DAY - YEAR (As seen on Website)
+    [SerializedField] public String[] artworkURLs; //Temporary, may change this logic later. Used for multiple
+    [SerializedField] public int artworkCount; //Use this field to display the number of pieces of artwork
+    [SerializedField] public int artworkID; //Use this field to track the ID number of the artwork (This is for our use)
+}
