2b85aff5a85522222cf4a7c4604d2ab1c41c15c1
Obikas <cade@tannernet.net>
2025-10-06 18:36:01 -0700

    Changed the artwork urls to a list and the file pathing

--- Assets/Scripts/ArtworkData.cs
+++ Assets/Scripts/ArtworkData.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "NewScriptableObjectScript", menuName = "Scriptable Objects/NewScriptableObjectScript")]
-public class NewScriptableObjectScript : ScriptableObject
+[CreateAssetMenu(fileName = "ArtworkData", menuName = "Scriptable Objects/ArtworkData")]
+public class ArtworkData : ScriptableObject
 {
     [SerializedField] public String artworkName; //Use this field for the artwork's name
     [SerializedField] public String artistName; //Use this field for the artist's name
@@ -9,7 +9,7 @@ public class NewScriptableObjectScript : ScriptableObject
     [SerializedField] public String genre; //Use this field for the artwork genre
     [SerializedField] public String artistInformation; //Use this field for any extra information about the artist
     [SerializedField] public String artworkDate; //Use this field for MONTH - DAY - YEAR (As seen on Website)
-    [SerializedField] public String[] artworkURLs; //Temporary, may change this logic later. Used for multiple
+    [SerializedField] public List<String> artworkURLs; //Temporary, may change this logic later. Used for multiple urls for artwork
     [SerializedField] public int artworkCount; //Use this field to display the number of pieces of artwork
     [SerializedField] public int artworkID; //Use this field to track the ID number of the artwork (This is for our use)
 }
