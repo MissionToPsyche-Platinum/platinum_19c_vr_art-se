8cff2f89ce831a4d57514e1656ef505f23bdae4e
Kyle Burnett <kylethompsonburnett@gmail.com>
2025-10-09 21:52:03 -0700

    US1-Teleportation, Task 42: Added functionality to fade to black before teleportation for comfort.

--- Assets/Scripts/Locomotion/Teleport/TeleportVisual.cs
+++ Assets/Scripts/Locomotion/Teleport/TeleportVisual.cs
@@ -1,9 +1,16 @@
+using System.Threading.Tasks;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TeleportVisual : MonoBehaviour
 {
     public LineRenderer lineRenderer;
     public GameObject landingSpot;
+    public Image blackScreen;
+
+    public enum FadeState { IDLE, INCREASING, DECREASING };
+    public FadeState fadeState = FadeState.IDLE;
+
     Vector3 target = new Vector3();   
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -14,7 +21,7 @@ public class TeleportVisual : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    async void Update()
     {
         if (lineRenderer != null && lineRenderer.enabled)
         {
@@ -35,6 +42,37 @@ public class TeleportVisual : MonoBehaviour
 
             landingSpot.transform.position = target;
         }
+
+        if (fadeState == FadeState.INCREASING)
+        {
+            Color color = blackScreen.color;
+            color.a += Time.deltaTime / LocomotionSettings.TELEPORT_FADE_TIME;
+            color.a = Mathf.Clamp(color.a, 0, 1);
+            blackScreen.color = color;
+
+            if(color.a == 1)
+            {
+                await Task.Delay((int)(LocomotionSettings.TELEPORT_FADE_WAIT * 1000));
+                fadeState = FadeState.DECREASING;
+            }
+        } 
+        else if(fadeState == FadeState.DECREASING)
+        {
+            Color color = blackScreen.color;
+            color.a -= Time.deltaTime / LocomotionSettings.TELEPORT_FADE_TIME;
+            color.a = Mathf.Clamp(color.a, 0, 1);
+            blackScreen.color = color;
+            
+            if (color.a == 0)
+            {
+                fadeState = FadeState.IDLE;
+            }
+        }
+    }
+
+    public void FadeOut()
+    {
+        fadeState = FadeState.INCREASING;
     }
 
     public void SetTarget(Vector3 target)
@@ -48,6 +86,6 @@ public class TeleportVisual : MonoBehaviour
     public void Unvisualize()
     {
         lineRenderer.enabled = false;
-        landingSpot.SetActive(false); 
+        landingSpot.SetActive(false);
     }
 }
