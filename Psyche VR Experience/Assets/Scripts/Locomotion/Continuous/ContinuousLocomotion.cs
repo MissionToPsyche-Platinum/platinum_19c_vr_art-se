5a30e7317ae09c049339053536371cd1523b97dd
rwalters44 <ryan.t.walters.044@gmail.com>
2025-11-03 22:43:32 -0700

    US90 Task 126: Initial Commit for Continuous Locomotion

--- Psyche VR Experience/Assets/Scripts/Locomotion/Continuous/ContinuousLocomotion.cs
+++ Psyche VR Experience/Assets/Scripts/Locomotion/Continuous/ContinuousLocomotion.cs
@@ -1,7 +1,15 @@
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit.Inputs.Readers;
 
 public class ContinuousLocomotion : MonoBehaviour
 {
+    public Transform rig;
+    public Transform cameraTransform;
+    public float moveSpeed = 2f;
+
+    [SerializeField]
+    XRInputValueReader<Vector2> inputDir;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -16,6 +24,19 @@ public class ContinuousLocomotion : MonoBehaviour
             return;
         }
 
-        // TODO : Task 126
+        Vector2 input = inputDir.ReadValue();
+
+        // skip if there is not much input
+        if (input.magnitude > 0.01f)
+        {
+            Vector3 movementDirection = new Vector3(input.x, 0f, input.y);
+            Vector3 cameraDirection = new Vector3(cameraTransform.forward.x, 0f, cameraTransform.forward.z).normalized;
+            
+            // rotate the movement to be in the direction of the camera
+            movementDirection = Quaternion.LookRotation(cameraDirection) * movementDirection;
+
+
+            rig.position += movementDirection * moveSpeed * Time.deltaTime;
+        }
     }
 }
