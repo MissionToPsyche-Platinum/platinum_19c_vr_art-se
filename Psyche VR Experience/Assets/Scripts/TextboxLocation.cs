7c91dd46395d1d257a769c7b7c7305c3c8ff3d45
EmilySanders123 <112529774+EmilySanders123@users.noreply.github.com>
2025-11-06 21:16:42 -0700

    Task 132: Fixed textbox positioning.

--- Psyche VR Experience/Assets/Scripts/TextboxLocation.cs
+++ Psyche VR Experience/Assets/Scripts/TextboxLocation.cs
@@ -8,11 +8,13 @@ public class TextboxLocation : MonoBehaviour
     void Start()
     {
         textbox.transform.position = new Vector3(frameBottom.transform.position.x, frameBottom.transform.position.y - (float)0.25, frameBottom.transform.position.z + (float)0.01);
+        textbox.transform.eulerAngles = new Vector3(0, frameBottom.transform.eulerAngles.y + (float)180.0, 0);
     }
 
     // Update is called once per frame
     void Update()
     {
         textbox.transform.position = new Vector3(frameBottom.transform.position.x, frameBottom.transform.position.y - (float)0.25, frameBottom.transform.position.z + (float)0.01);
+        textbox.transform.eulerAngles = new Vector3(0, frameBottom.transform.eulerAngles.y + (float)180.0, 0);
     }
 }
