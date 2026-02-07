2a217becb6dc64b54d8b8eeb9fa854f6e4f49273
rwalters44 <ryan.t.walters.044@gmail.com>
2026-02-06 19:51:40 -0700

    US242 Task 267: Added wristwatch and created script to get remaining time from expo timer [UNTESTED]

---
+++ Psyche VR Experience/Assets/Scripts/Expo/WatchTimer.cs
@@ -0,0 +1,21 @@
+using TMPro;
+using UnityEngine;
+
+public class WatchTimer : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] private ExpoTimer expoTimer;
+    [SerializeField] private TextMeshProUGUI watchText;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float seconds = expoTimer.getSecondsLeft();
+        watchText.text = ((int) seconds).ToString();
+    }
+}
