bcd0361871bdd3be69e0eeb7f0caa663009f96e5
Obikas <cade@tannernet.net>
2026-03-04 00:55:56 -0700

    Task 369a-Added a fadeout, material is not working will fix later

---
+++ Psyche VR Experience/Assets/Scripts/Locomotion/FadeOutHandler.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using Unity Engine;
+
+public class FadeOutHandler : MonoBehavior
+{
+    [SerializeField] private float fadeToBlackDelay = 0.05f;
+    private Material material;
+    private bool fadeActive = false;
+
+    private void Start()
+    {
+        material = GetComponent<MeshRenderer>().material;
+    }
+
+    public void Fade(bool fadeOut)
+    {
+        if (fadeOut && fadeActive)
+        {
+            return;
+        }
+        else if (!fadeOut && fadeActive)
+        {
+            return;
+        }
+
+        fadeActive = fadeOut;
+        Debug.Log("Fade is activating");
+        StartCoroutine(FadeEffect(fadeOut));
+    }
+
+    private IEnumerator FadeEffect(bool fadeOut)
+    {
+        float startAlpha = material.GetFloat("Alpha");
+        float endAlpha = fadeOut ? 1.0f : 0.0f;
+        float remainingTime = fadeToBlackDelay * Mathf.Abs(endAlpha - StartAlpha);
+        float elapsedTime = 0;
+        while (elapsedTime < fadeToBlackDelay)
+        {
+            elapsedTime += Time.deltaTime;
+            float tempVal = MathF.Lerp(startAlpha, endAlpha, elapsedTime / remainingTime);
+            material.SetFloat("Alpha", tempVal);
+            yield return null;
+        }
+        material.SetFloat("Alpha", endAlpha);
+    }
+}
